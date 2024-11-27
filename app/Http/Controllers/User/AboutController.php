665b5e00ce4dc46ad6855187e7efbff7b5e1994e
manikbayu23 <gdmanikmas@gmail.com>
2024-11-27 12:13:21 +0800

    update frontend

--- app/Http/Controllers/User/AboutController.php
+++ app/Http/Controllers/User/AboutController.php
@@ -8,14 +8,21 @@ use App\Http\Controllers\Controller;
 
 class AboutController extends Controller
 {
-    public function index()
+    public function history()
     {
         try {
             return view(
-                'user.pages.about-us',
-                [
-                    'title' => 'Tentang Kami'
-                ]
+                'user.pages.about-us-history'
+            );
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+        }
+    }
+    public function meaning()
+    {
+        try {
+            return view(
+                'user.pages.about-us-meaning'
             );
         } catch (\Throwable $th) {
             Log::error($th->getMessage());
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,24 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminController;
-use App\Http\Controllers\User\HomeController as UserHome;
 use App\Http\Controllers\User\ActivityController as UserActivity;
 use App\Http\Controllers\User\ContactController as UserContact;
 use App\Http\Controllers\User\HelpController as UserHelp;
 use App\Http\Controllers\User\QuisController as UserQuis;
 use App\Http\Controllers\User\AboutController as UserAbout;
+use App\Http\Controllers\User\BerandaController as UserBeranda;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [UserHome::class, 'index'])->name('/');
+Route::get('/', [UserBeranda::class, 'index'])->name('/');
 Route::get('/activity', [UserActivity::class, 'index'])->name('activity');
 Route::get('/contact-person', [UserContact::class, 'index'])->name('contact-person');
 Route::get('/help', [UserHelp::class, 'index'])->name('help');
 Route::get('/quis', [UserQuis::class, 'index'])->name('quis');
-Route::get('/about-us', [UserAbout::class, 'index'])->name('about-us');
+Route::prefix('/about-us')->group(function () {
+    Route::get('/history', [UserAbout::class, 'history'])->name('about-us.history');
+    Route::get('/meaning', [UserAbout::class, 'meaning'])->name('about-us.meaning');
+});
 
-
-Route::prefix('admin')->group(function(){
+Route::prefix('admin')->group(function () {
     Route::get('/', [AdminController::class, 'index'])->name('admin.dashboard');
 });
